915cad8602d4f8f28375c45f21b260180463f1d4
Max Chernoff <git@maxchernoff.ca>
2026-02-18 02:09:55 -0700

    More Python

--- scripts/generate-database.py
+++ scripts/generate-database.py
@@ -11,14 +11,14 @@
 ###############
 
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
-from gzip import decompress as gzip_decompress
+from gzip import compress as gzip_compress, decompress as gzip_decompress
 from lzma import decompress as lzma_decompress
 from pathlib import Path
 from pprint import pp as pprint
 from re import MULTILINE, VERBOSE, compile as re_compile
 from sys import exit, stderr
 from time import time
-from typing import Literal, NamedTuple
+from typing import Literal, NamedTuple, TypeAlias
 from urllib.request import urlopen
 from zipfile import ZIP_DEFLATED, ZipFile, ZipInfo
 
@@ -29,7 +29,7 @@ from zipfile import ZIP_DEFLATED, ZipFile, ZipInfo
 
 
 class FileEntry(NamedTuple):
-    """A file entry in the database."""
+    """A file entry in the filename list sources."""
 
     filename: str
     """The name of the file, without any path components."""
@@ -49,11 +49,41 @@ class FileEntry(NamedTuple):
     unknown."""
 
 
+class CTANRow(NamedTuple):
+    """A row in the exported filename database for a file from CTAN."""
+
+    path: str
+    """The path of the file, relative to the root of the CTAN archive."""
+
+    revision: int
+    """The SVN revision of the file, as an integer."""
+
+
+class ZipRow(NamedTuple):
+    """A row in the exported filename database for a file from the zip file."""
+
+    path: str
+    """The name of the file inside the zip file."""
+
+    revision: int
+    """The SVN revision of the file, as an integer."""
+
+    start_offset: int
+    """The byte offset of the start of gzip-compressed file data in the zip file."""
+
+    end_offset: int
+    """The byte offset of the end of gzip-compressed file data in the zip file."""
+
+
+DatabaseRow: TypeAlias = CTANRow | ZipRow
+FileList: TypeAlias = dict[str, list[FileEntry]]
+
 #################
 ### Constants ###
 #################
 
 __VERSION__ = "0.1.2"  # %%version
+DATABASE_FILENAME = "network-install.files.lut.gz"
 DEFAULT_MIRROR = "https://mirror.ctan.org"
 FILES_PATH = "/FILES.byname.gz"
 IGNORE_EXTENSIONS = {"pdf", "png", "jpg", "4ht"}
@@ -61,8 +91,9 @@ IGNORE_PREFIXES = {"lwarp"}
 START_TIME = time()
 TLPDB_PATH = "/systems/texlive/tlnet/tlpkg/texlive.tlpdb.xz"
 TLPDB_REVISION_REGEX = re_compile(r"^revision (\d+)\s*$", MULTILINE)
-ZIP_NAME = "network-install.files.zip"
 ZIP_EPOCH = (1980, 1, 1, 0, 0, 0)
+ZIP_LOCAL_FILE_HEADER_SIZE = 30
+ZIP_NAME = "network-install.files.zip"
 
 TLPDB_FILE_REGEX = re_compile(
     r"""
@@ -70,13 +101,24 @@ TLPDB_FILE_REGEX = re_compile(
         (texmf-dist | RELOC ) /
 
         (?P<path>
-            # We only care about files in the $TEXMFDIST/tex/ directory, since
-            # everything else should be included unpacked in the CTAN archive.
-            tex/
+            # We only care about files in the following subdirectories.
+            (?P<format>
+                dvips |  # For PSTricks
+                metapost |  # For luamplib
+                scripts |  # Maybe some of the stuff here is used at runtime?
 
-            # The network-install package is LuaLaTeX-only, so we only care
-            # about the following formats.
-            (?: latex | generic | lualatex | luatex )/
+                # TeX
+                tex / (?:
+                    latex | generic | lualatex | luatex
+                ) |
+
+                # Fonts
+                fonts / (?:
+                    tfm | type1 | vf | afm | enc | # Type 1 fonts
+                    cmap | map |  # Map files
+                    opentype | truetype # OpenType and TrueType fonts
+                ) |
+            ) /
 
             # Match the rest of the path
             \S*?
@@ -178,18 +220,25 @@ def download_files(mirror_url: str) -> tuple[str, str]:
     return files_byname, tlpdb
 
 
-def filelist_from_tlpdb(tlpdb: str) -> dict[str, list[FileEntry]]:
+def filelist_from_tlpdb(
+    tlpdb: str,
+) -> tuple[FileList, FileList]:
     """Extract the filelist from the tlpdb file.
 
     Args:
         tlpdb: The contents of the tlpdb file as a string.
 
     Returns:
-        A dictionary mapping file names to their corresponding FileEntry
-        objects.
+        A tuple of two dictionaries mapping file names to their corresponding
+        FileEntry objects.
+
+        The first dictionary contains _all_ files that could potentially be used
+        at runtime; the second dictionary contains only the files that should be
+        included in the zip file if they're missing from the CTAN filelist.
     """
 
-    files: dict[str, list[FileEntry]] = {}
+    all_files: FileList = {}
+    zip_files: FileList = {}
 
     for package in tlpdb.split("\n\n"):
         revision_match = TLPDB_REVISION_REGEX.search(package)
@@ -201,26 +250,30 @@ def filelist_from_tlpdb(tlpdb: str) -> dict[str, list[FileEntry]]:
         for file_match in TLPDB_FILE_REGEX.finditer(package):
             filename = file_match.group("filename")
             path = file_match.group("path")
-
-            if filename not in files:
-                files[filename] = []
-
-            files[filename].append(
-                FileEntry(
-                    filename=filename,
-                    path=path,
-                    source="tlpdb",
-                    date=None,
-                    revision=revision,
-                )
+            entry = FileEntry(
+                filename=filename,
+                path=path,
+                source="tlpdb",
+                date=None,
+                revision=revision,
             )
 
-    return files
+            if filename not in all_files:
+                all_files[filename] = []
+
+            all_files[filename].append(entry)
+
+            if file_match.group("format").startswith("tex/"):
+                if filename not in zip_files:
+                    zip_files[filename] = []
+                zip_files[filename].append(entry)
+
+    return all_files, zip_files
 
 
 def filelist_from_ctan(
     files_byname: str,
-) -> dict[str, list[FileEntry]]:
+) -> FileList:
     """Extract the filelist from the FILES.byname file.
 
     Args:
@@ -231,14 +284,20 @@ def filelist_from_ctan(
         objects.
     """
 
-    files: dict[str, list[FileEntry]] = {}
+    files: FileList = {}
 
     for file_match in CTAN_FILE_REGEX.finditer(files_byname):
+        # Parse the file information from the regex match
         filename = file_match.group("filename")
         path = file_match.group("path")
         date_str = file_match.group("date")
         date = int(date_str.replace("/", ""))
 
+        # Special case: exclude the latex-dev files
+        if path.startswith("macros/latex-dev/"):
+            continue
+
+        # Add the file entry to the filelist
         if filename not in files:
             files[filename] = []
 
@@ -256,8 +315,8 @@ def filelist_from_ctan(
 
 
 def get_missing_ctan_files(
-    tlpdb_files: dict[str, list[FileEntry]],
-    ctan_files: dict[str, list[FileEntry]],
+    tlpdb_files: FileList,
+    ctan_files: FileList,
 ) -> set[FileEntry]:
     """Get the list of files that are in the tlpdb but not in the ctan filelist.
 
@@ -298,6 +357,105 @@ def get_missing_ctan_files(
         out.add(entries[0])
 
     return out
+
+
+def get_found_ctan_files(
+    tlpdb_files: FileList,
+    ctan_files: FileList,
+) -> dict[str, CTANRow]:
+    """Get the list of files that are in both the tlpdb and the ctan filelist.
+
+    Args:
+        tlpdb_files: The filelist extracted from the texlive.tlpdb file.
+        ctan_files: The filelist extracted from the FILES.byname file.
+
+    Returns:
+        A dictionary mapping file names to CTANRow objects representing the
+        files that are in both the tlpdb and the ctan filelist.
+    """
+    found = set(tlpdb_files.keys()).intersection(set(ctan_files.keys()))
+    out: dict[str, CTANRow] = {}
+
+    for filename in found:
+        # If there are multiple entries for this file, just ignore it since
+        # there's nothing sensible that we can do here.
+        if len(ctan_files[filename]) > 1 or len(tlpdb_files[filename]) > 1:
+            continue
+
+        # Get the entries for this file
+        ctan_entry = ctan_files[filename][0]
+        tlpdb_entry = tlpdb_files[filename][0]
+
+        # If the revision is missing (shouldn't be possible), fail with an error
+        if tlpdb_entry.revision is None:
+            raise ValueError(
+                f"Revision is missing for file {filename} in tlpdb filelist."
+            )
+
+        # Otherwise, add the single entry for this file to the output list.
+        out[filename] = CTANRow(
+            path=ctan_entry.path,
+            revision=tlpdb_entry.revision,
+        )
+
+    return out
+
+
+def get_zip_files(
+    tlpdb_files: FileList,
+    zip_path: Path,
+) -> dict[str, ZipRow]:
+    """Get the list of files that are in the zip file.
+
+    Args:
+        tlpdb_files: The filelist extracted from the texlive.tlpdb file.
+        zip_path: The path to the zip file containing the missing files.
+
+    Returns:
+        A dictionary mapping file names to ZipRow objects representing the files
+        that are in the zip file.
+    """
+
+    zip_files: dict[str, ZipRow] = {}
+
+    with ZipFile(zip_path, "r") as zip_file:
+        for zip_info in zip_file.infolist():
+            filename = zip_info.filename
+
+            # We only care about files that are in the tlpdb filelist, but it
+            # should be impossible for a file to be missing from there.
+            try:
+                tlpdb_entry = tlpdb_files[filename][0]
+            except KeyError:
+                raise ValueError(
+                    f"File {filename} is in the zip file but not in the tlpdb filelist."
+                )
+
+            # The revision should also be present in the tlpdb filelist
+            revision = tlpdb_entry.revision
+            if revision is None:
+                raise ValueError(
+                    f"Revision is missing for file {filename} in tlpdb filelist."
+                )
+
+            # Get the byte offsets of the compressed file data in the zip file.
+            start_offset = (
+                zip_info.header_offset
+                + ZIP_LOCAL_FILE_HEADER_SIZE
+                + len(zip_info.filename)
+                + len(zip_info.extra)
+            )
+            end_offset = start_offset + zip_info.compress_size
+
+            # Now, add the entry for this file to the output dictionary.
+            zip_files[filename] = ZipRow(
+                path=filename,
+                revision=revision,
+                start_offset=start_offset,
+                end_offset=end_offset,
+            )
+
+    return zip_files
 
 
 def create_zip(
@@ -377,6 +535,56 @@ def create_zip(
                 continue
 
 
+def save_database(
+    output_file: Path,
+    files: dict[str, DatabaseRow],
+    zip_path: Path,
+) -> None:
+    """Save the database to a compressed Lua table.
+
+    Args:
+        output_file: The path to the output Lua file.
+
+        files: The dictionary of DatabaseRow objects representing the files to
+            include in the database.
+
+        zip_path: The path to the zip file containing the missing files.
+    """
+
+    # Sort by revision and then by path to ensure deterministic output.
+    sorted_files = sorted(
+        files.items(),
+        key=lambda row: (
+            row[1].revision,
+            row[1].path,
+        ),
+    )
+
+    lines = [
+        "return {",
+    ]
+    for filename, row in sorted_files:
+        if isinstance(row, CTANRow):
+            lines.append(
+                f'["{filename}"]={{source="ctan",path="{row.path}",revision={row.revision}}},'
+            )
+        elif isinstance(row, ZipRow):
+            lines.append(
+                f'["{filename}"]={{source="zip",path="{row.path}",revision={row.revision},offset={{{row.start_offset},{row.end_offset}}}}},'
+            )
+        else:
+            raise ValueError(f"Invalid row type for file {filename}.")
+    lines.append("}")
+
+    # Join the lines into a single string and encode it as bytes.
+    data = "".join(lines).encode("utf-8")
+
+    # Compress the data using gzip and write it to the output file.
+    compressed_data = gzip_compress(data, compresslevel=9)
+    with output_file.open("wb") as f:
+        f.write(compressed_data)
+
+
 def run(
     output_directory: Path,
     mirror_url: str,
@@ -399,12 +607,12 @@ def run(
 
     # Extract the filelists
     ctan_files = filelist_from_ctan(ctan)
-    tlpdb_files = filelist_from_tlpdb(tlpdb)
-    ctan_missing = get_missing_ctan_files(tlpdb_files, ctan_files)
+    all_tlpdb_files, zip_tlpdb_files = filelist_from_tlpdb(tlpdb)
 
     if generate_zip:
         msg("Generating zip file...")
         # Get the list of files in TL but not in CTAN
+        ctan_missing = get_missing_ctan_files(zip_tlpdb_files, ctan_files)
 
         # Generate the zip file containing the missing files
         output_directory.mkdir(parents=True, exist_ok=True)
@@ -424,6 +632,18 @@ def run(
                 "Generating the database without generating the zip file is not supported since the database would be incomplete."
             )
         msg("Generating database...")
+
+        # Get the list of files needed
+        ctan_found = get_found_ctan_files(all_tlpdb_files, ctan_files)
+        zip_files = get_zip_files(zip_tlpdb_files, output_directory / ZIP_NAME)
+
+        # Generate the filename database and save it to a file.
+        save_database(
+            output_file=output_directory / DATABASE_FILENAME,
+            files=ctan_found | zip_files,
+            zip_path=output_directory / ZIP_NAME,
+        )
+        msg("Finished generating database.")
 
 
 ####################
